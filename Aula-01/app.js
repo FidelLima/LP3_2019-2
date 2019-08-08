6e40bfaee3c563dd70875e71a91fb051198a7682
“Fidel <cr.belga@gmail.com>
2019-08-08 19:06:35 -0400

    “CPF”

--- Aula-01/app.js
+++ Aula-01/app.js
@@ -19,11 +19,28 @@ app.use('/inverter/:str', (req,res) => {
     res.json(str);
  });
 
-app.use('/cpf:cpf', (req,res) => {
-   let cpf = req.params.cpd;
-   // tomamo no cu
-   res.setDefaultEncoding('Validador de CPF')
- });
+ app.use('/cpf/:cpf', (req, res) => { 
+    let cpf = req.params.cpf;
+    let som = 0;
+    let cod;
+    som = 0;
+    //cpf com valores iguais 
+  if (cpf == "00000000000") res.send(false);
+
+  for (i=1; i<=9; i++) som = som + parseInt(cpf.substring(i-1, i)) * (11 - i);
+  cod = (som * 10) % 11;
+   
+    if ((cod == 10) || (cod == 11))  cod = 0;
+    if (cod != parseInt(cpf.substring(9, 10)) ) res.send(false);
+   
+    som = 0;
+    for (i = 1; i <= 10; i++) som = som + parseInt(cpf.substring(i-1, i)) * (12 - i);
+    cod = (som * 10) % 11;
+   
+    if ((cod == 10) || (cod == 11))  cod = 0;
+    if (cod != parseInt(cpf.substring(10, 11) ) ) res.send(false);
+    res.send(true);
+});
 
 
 
