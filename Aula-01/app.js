67d9b8938c3ccdd3d05bd484bc9025d8c65117dc
“fidel” <cr.belga@gmail.com>
2019-08-09 22:59:57 -0400

    “Atualizado

--- Aula-01/app.js
+++ Aula-01/app.js
@@ -16,7 +16,7 @@ app.use('/inverter/:str', (req,res) => {
     .split('')
     .reverse()
     .join('');
-    res.json(str);
+    res.json({resultado: str});
  });
 
  app.use('/cpf/:cpf', (req, res) => { 
@@ -25,25 +25,21 @@ app.use('/inverter/:str', (req,res) => {
     let cod;
     som = 0;
     //cpf com valores iguais 
-  if (cpf == "00000000000") res.send(false);
+  if (cpf == "00000000000") res.json({valido:false});
 
   for (i=1; i<=9; i++) som = som + parseInt(cpf.substring(i-1, i)) * (11 - i);
   cod = (som * 10) % 11;
    
     if ((cod == 10) || (cod == 11))  cod = 0;
-    if (cod != parseInt(cpf.substring(9, 10)) ) res.send(false);
+    if (cod != parseInt(cpf.substring(9, 10)) ) res.json({valido:false});
    
     som = 0;
     for (i = 1; i <= 10; i++) som = som + parseInt(cpf.substring(i-1, i)) * (12 - i);
     cod = (som * 10) % 11;
    
     if ((cod == 10) || (cod == 11))  cod = 0;
-    if (cod != parseInt(cpf.substring(10, 11) ) ) res.send(false);
-    res.send(true);
+    if (cod != parseInt(cpf.substring(10, 11) ) ) res.json({valido:false});
+    res.json({valido:true});
 });
 
-
-
-app.listen(3000,
-    () => console.log('Servidor iniciado')
-    );
+module.exports=app; //esporta 
