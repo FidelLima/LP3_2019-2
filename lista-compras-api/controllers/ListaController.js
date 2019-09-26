6052205e6f43c33742de5db9109de3493e68800a
fidel <cr.belga@gmail.com>
2019-09-26 19:30:37 -0400

    Add serviço de recuperação de itens

---
+++ lista-compras-api/controllers/ListaController.js
@@ -0,0 +1,53 @@
+/**
+ * Este import é um exemplo de 
+ * associação por desestruturação
+ */
+const { Op } = require('sequelize');
+const { Lista, Item } = require('../databases/db');
+
+const controller = {
+    // Arrow function
+    recuperarTodas: async (req, res) => {
+        const listas = await Lista.findAll();
+        return res.json(listas);
+    },
+
+    salvar: (req, res) => {
+        const lista = req.body;
+
+        if (!lista.nome) {
+            return res
+                .status(400)
+                .json({ mensagem: 'Nome não informado' });
+        }
+
+        Lista
+            .create(lista)
+            .then(
+                listaSalva => res.status(201).json(listaSalva),
+                erro => res.status(400).json(erro)
+            )
+            .catch(erro => {
+                console.log(erro);
+                return res
+                    .status(500)
+                    .json({ mensagem: 'Erro ao tentar salvar a lista' });
+            });
+    },
+
+    recuperarItensPorDescricao: async (req, res) => {
+        //const consulta = req.body.consulta;
+        const { consulta } = req.body;
+        let itens = await Item.findAll({
+            where: {
+                descricao: {
+                    [Op.iLike]: `%${consulta}%`
+                }
+            }
+        });
+
+        return res.json(itens);
+    }
+};
+
+module.exports = controller;
---
+++ lista-compras-api/databases/db.js
@@ -0,0 +1,36 @@
+const Sequelize = require('sequelize');
+
+const dbConfig = require('./config');
+const ListaModel = require('../models/ListaModel');
+const ItemModel = require('../models/ItemModel');
+
+const modo = process.env.NODE_ENV || 'development';
+const config = dbConfig[modo];
+
+const conexao = new Sequelize(
+    config.db,
+    config.user,
+    config.password, {
+    host: config.host,
+    dialect: config.dialect,
+    port: config.port
+}
+);
+
+/**
+ * Você precisa inicializar todos os modelos
+ * antes da sincronização que ocorre
+ * logo abaixo!
+ */
+
+const Lista = ListaModel(conexao, Sequelize);
+const Item = ItemModel(conexao, Sequelize);
+
+conexao
+    .sync({ alter: true })
+    .then(() => console.log('BD conectado e sincronizado'));
+
+const db = { Lista, Item };
+
+module.exports = db;
+
